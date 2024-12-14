aceabd82836e1ffe1fc32751802c89f96796e734
Udnek <gleb.dudorov@gmail.com>
2024-12-15 00:27:28 +0300

    inventories

--- src/main/java/me/udnek/itemscoreu/custominventory/ConstructableCustomInventory.java
+++ src/main/java/me/udnek/itemscoreu/custominventory/ConstructableCustomInventory.java
@@ -3,6 +3,7 @@ package me.udnek.itemscoreu.custominventory;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class ConstructableCustomInventory implements CustomInventory{
@@ -19,6 +20,16 @@ public abstract class ConstructableCustomInventory implements CustomInventory{
     public void generateInventory(int size, Component title){
         inventory = Bukkit.createInventory(this, size, title);
     }
+
+    public void addItem(int slot, int amount){
+        ItemStack item = getInventory().getItem(slot);
+        if (item == null) return;
+        getInventory().setItem(slot, item.add(amount));
+    }
+    public void takeItem(int slot, int amount){
+        addItem(slot, -amount);
+    }
+
 
     @NotNull
     @Override
