472d77ffe0cda73976b3770cdad561a2be41ce9f
Udnek <gleb.dudorov@gmail.com>
2025-01-18 15:35:05 +0300

    fixed rounding

--- src/main/java/me/udnek/itemscoreu/util/Utils.java
+++ src/main/java/me/udnek/itemscoreu/util/Utils.java
@@ -4,15 +4,16 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import java.util.function.Consumer;
 
 public class Utils {
 
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.US));
+
     public static @NotNull String roundToTwoDigits(double value){
-        if (value % 1 == 0){
-            return String.valueOf((int) value);
-        }
-        return new DecimalFormat("#.##").format(value);
+        return DECIMAL_FORMAT.format(value);
     }
 
     public static <T> void consumeIfNotNull(@Nullable T object, @NotNull Consumer<@NotNull T> consumer){
