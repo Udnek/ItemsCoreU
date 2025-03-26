cd6bfbb51c07e96c3d06532453ad752a527c0eea
PUBKABOZKAX <g_churakov@list.ru>
2025-03-27 01:31:03 +0300

    DispensableItem (ALWAYS_DROP)

--- src/main/java/me/udnek/itemscoreu/customcomponent/instance/DispensableItem.java
+++ src/main/java/me/udnek/itemscoreu/customcomponent/instance/DispensableItem.java
@@ -3,13 +3,36 @@ package me.udnek.itemscoreu.customcomponent.instance;
 import me.udnek.itemscoreu.customcomponent.CustomComponent;
 import me.udnek.itemscoreu.customcomponent.CustomComponentType;
 import me.udnek.itemscoreu.customitem.CustomItem;
+import org.bukkit.block.Block;
+import org.bukkit.block.Dispenser;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public interface DispensableItem extends CustomComponent<CustomItem> {
     DispensableItem DEFAULT = new DispensableItem() {
         @Override
         public void onDispense(@NotNull CustomItem item, @NotNull BlockDispenseEvent event) {}
+
+        @Override
+        public void onDrop(@NotNull CustomItem item, @NotNull BlockDispenseEvent event) {}
+    };
+
+    DispensableItem ALWAYS_DROP = new DispensableItem() {
+        @Override
+        public void onDispense(@NotNull CustomItem item, @NotNull BlockDispenseEvent event) {
+            Block block = event.getBlock();
+            Item entity = (Item) block.getWorld().spawnEntity(block.getLocation(), EntityType.ITEM);
+            entity.setVelocity(event.getVelocity());
+            ItemStack itemStack = event.getItem();
+            entity.setItemStack(itemStack);
+            Inventory inventory = ((Dispenser) block.getState()).getInventory();
+            int firstItem = inventory.first(itemStack);
+            inventory.setItem(firstItem, itemStack.getAmount() == 1 ? null : itemStack.add(-1));
+        }
 
         @Override
         public void onDrop(@NotNull CustomItem item, @NotNull BlockDispenseEvent event) {}
