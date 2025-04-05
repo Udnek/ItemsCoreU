ed1f31445cb3ab5a0a64f5d4157592dee3e0085d
PUBKABOZKAX <g_churakov@list.ru>
2025-04-05 17:04:00 +0300

    CustomEntityType.consumeIfCustom

--- src/main/java/me/udnek/itemscoreu/util/Utils.java
+++ src/main/java/me/udnek/itemscoreu/util/Utils.java
@@ -1,11 +1,16 @@
 package me.udnek.itemscoreu.util;
 
+import io.papermc.paper.math.Position;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.data.BlockData;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class Utils {
@@ -14,6 +19,14 @@ public class Utils {
 
     public static @NotNull String roundToTwoDigits(double value){
         return DECIMAL_FORMAT.format(value);
+    }
+
+    public static void sendBlockDamageForAllPlayers(@NotNull Location location, float progress) {
+        location.getWorld().getPlayers().forEach(player -> player.sendBlockDamage(location, progress));
+    }
+
+    public static void sendMultiBlockDamageForAllPlayers(@NotNull World world, @NotNull Map<? extends Position, BlockData> blockChanges) {
+        world.getPlayers().forEach(player -> player.sendMultiBlockChange(blockChanges));
     }
 
     public static <T> void consumeIfNotNull(@Nullable T object, @NotNull Consumer<@NotNull T> consumer){
